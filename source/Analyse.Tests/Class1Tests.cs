fcd0485eb39abe5d1c93d93b89ecd0c9943507cb
paul.jones <paul.jones@animalfriends.co.uk>
2022-10-07 10:24:46 +0100

    Initial setup of code base

---
+++ source/Analyse.Tests/Class1Tests.cs
@@ -0,0 +1,13 @@
+﻿namespace DemoLibrary.Tests
+{
+    /// <summary>
+    /// Tests for the <see cref="Class1"/> class.
+    /// </summary>
+    public class Class1Tests
+    {
+        public void Add_WithInput_ReturnsSum()
+        {
+
+        }
+    }
+}
